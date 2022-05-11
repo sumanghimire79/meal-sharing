1ea3b74558c2905bf28e66b6f6de55026cc1fe78
sumanghimire79 <sumanghimire79@yahoo.com>
2022-05-12 01:24:12 +0200

    if/else used inside selfcalling annonymous function in home page

--- src/client/components/Home.js
+++ src/client/components/Home.js
@@ -12,7 +12,7 @@ export function Home() {
 
   useEffect(() => {
     fetchItem();
-  }, [fetchItem]);
+  }, []);
 
   const popular = popularMeals.map((popularMeal, index) => {
     return (
@@ -20,37 +20,42 @@ export function Home() {
         <Link to={`/meals/${popularMeal.id}`}>
           <h3>{popularMeal.title}</h3>
 
-          {popularMeal.title.toLowerCase().includes('pizza') && (
-            <img
-              className="bestItem"
-              src="https://cdn.pixabay.com/photo/2017/12/09/08/18/pizza-3007395__480.jpg"
-              alt="Pizza"
-            />
-          )}
-          {popularMeal.title.toLowerCase().includes('pita') && (
-            <img
-              className="bestItem"
-              src="https://mandekogebogen.dk/image/box/2130725/980/50000.jpg?oversize=1"
-              alt="Pita Bread"
-            />
-          )}
-
-          {popularMeal.title.toLowerCase().includes('sandwitch') && (
-            <img
-              className="bestItem"
-              src="https://www.bing.com/th?id=OIP.8RJptF5WHZYS6JYr9jXS1QHaFH&w=300&h=207&c=8&rs=1&qlt=90&o=6&dpr=1.5&pid=3.1&rm=2"
-              alt="sandwitch"
-            />
-          )}
-          {!popularMeal.title
-            .toLowerCase()
-            .includes('sandwitch' || 'pita' || 'pizza') && (
-            <img
-              className="bestItem"
-              src="https://gastrofun.dk/wp-content/uploads/2020/04/Hjemmelavet-Burger-Shack-Burger-1.jpg"
-              alt="bestItem"
-            />
-          )}
+          {(() => {
+            if (popularMeal.title.toLowerCase().includes('pizza')) {
+              return (
+                <img
+                  className="bestItem"
+                  src="https://cdn.pixabay.com/photo/2017/12/09/08/18/pizza-3007395__480.jpg"
+                  alt="Pizza"
+                />
+              );
+            } else if (popularMeal.title.toLowerCase().includes('pita')) {
+              return (
+                <img
+                  className="bestItem"
+                  src="https://mandekogebogen.dk/image/box/2130725/980/50000.jpg?oversize=1"
+                  alt="Pita Bread"
+                />
+              );
+            }
+            if (popularMeal.title.toLowerCase().includes('sandwitch')) {
+              return (
+                <img
+                  className="bestItem"
+                  src="https://www.bing.com/th?id=OIP.8RJptF5WHZYS6JYr9jXS1QHaFH&w=300&h=207&c=8&rs=1&qlt=90&o=6&dpr=1.5&pid=3.1&rm=2"
+                  alt="sandwitch"
+                />
+              );
+            } else {
+              return (
+                <img
+                  className="bestItem"
+                  src="https://static.vecteezy.com/system/resources/previews/000/095/259/original/restaurant-menu-vector.jpg"
+                  alt="bestItem"
+                />
+              );
+            }
+          })()}
         </Link>
         <p>
           <strong> {popularMeal.location} </strong>
